35e24ed240c18aa244d4952f807d1c7f012e2571
thaianhnv99 <thaianhnguyen283@gmail.com>
2024-03-07 12:42:57 +0700

    init

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,13 +1,14 @@
 /* eslint-disable @next/next/no-sync-scripts */
 import Script from "next/script";
+var deselectCurrent = require("toggle-selection");
 
 const Home = () => {
   return (
     <>
-      <Script
+      {/* <Script
         crossOrigin="anonymous"
         src="https://cdn.jsdelivr.net/npm/copy-to-clipboard@3.3.3/index.min.js"
-      />
+      /> */}
       <script type="text/javascript" src="/static/script.js" />
       <div>Show home</div>
     </>
