621e5b15993e7b0b2fcb53d32163794026a62a64
thaianhnv99 <thaianhnguyen283@gmail.com>
2024-03-07 14:26:12 +0700

    init

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,5 +1,6 @@
 /* eslint-disable @next/next/no-sync-scripts */
 import copy from "copy-to-clipboard";
+import Head from "next/head";
 import Script from "next/script";
 
 const Home = () => {
@@ -11,10 +12,12 @@ const Home = () => {
   };
   return (
     <>
-      <script
-        type="text/javascript"
-        src="https://cdn.jsdelivr.net/npm/copy-to-clipboard@3.3.3/index.min.js"
-      />
+      <Head>
+        <script
+          type="text/javascript"
+          src="https://cdn.jsdelivr.net/npm/copy-to-clipboard@3.3.3/index.min.js"
+        />
+      </Head>
       <script type="text/javascript" src="/static/script.js" />
       <div>
         Show home <button onClick={handleClick}>Copy</button>
